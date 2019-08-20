7c0dbb8ae03fa109f384b60ec166888366c1f24a
Administrator <379805167@qq.com>
2019-08-20 15:06:52 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World version5.0");
+        System.out.println("Hello World version6.0");
     }
 }
