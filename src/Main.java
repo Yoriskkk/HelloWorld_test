cdc4482f8d8b0e6d0ed97839051298008dd83100
Administrator <379805167@qq.com>
2019-08-20 15:04:36 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World version4.0");
+        System.out.println("Hello World version5.0");
     }
 }
